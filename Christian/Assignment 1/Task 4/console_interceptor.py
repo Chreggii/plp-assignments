ee7fa7c097f941c976e7a99d3d67ff9b06eeebce
Christian Omlin <christian.omlin@siemens.com>
2023-03-15 16:43:26 +0100

    refactor(Task 4): move console interceptor in own class

---
+++ Christian/Assignment 1/Task 4/console_interceptor.py
@@ -0,0 +1,42 @@
+from encoder import Encoder
+from decode import Decoder
+
+
+class ConsoleInterceptor:
+
+    def process_console_input():
+
+        is_quit = False
+
+        while not is_quit:
+            input_string = input("> ")
+            if input_string == 'quit':
+                is_quit = True
+                break
+            else:
+                try:
+                    splitted_input = input_string.split()
+
+                    if len(splitted_input) >= 3:
+
+                        if splitted_input[0] == 'encode':
+                            # Encode
+                            additional_string = splitted_input[3] if len(
+                                splitted_input) == 4 else None
+                            encoder = Encoder(
+                                splitted_input[1], int(splitted_input[2]), additional_string)
+                            encoder_result = encoder.encode_string()
+                            print(encoder_result)
+
+                        elif splitted_input[0] == 'decode':
+                            # Decode
+                            decoder = Decoder(
+                                splitted_input[1], int(splitted_input[2]))
+                            print(decoder.decode_string())
+
+                        else:
+                            raise
+                    else:
+                        raise
+                except:
+                    print('Invalid Input!')
